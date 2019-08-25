85e1a7df3b031d4967ea9ebf6ed8f383a74bed89
naren-mehta <deepakmehta.singh@gmail.com>
2019-08-25 16:43:13 +0530

    Working on REST api for customer

---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/exception/handler/CustomerNotFoundException.java
@@ -0,0 +1,25 @@
+package com.luv2code.springdemo.exception.handler;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+	public CustomerNotFoundException() {
+		super();
+	}
+
+	public CustomerNotFoundException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+	}
+
+	public CustomerNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public CustomerNotFoundException(String message) {
+		super(message);
+	}
+
+	public CustomerNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+}
---
+++ spring-crm-rest-demo/src/main/java/com/luv2code/springdemo/exception/handler/CustomerRestExceptionHandler.java
@@ -0,0 +1,31 @@
+package com.luv2code.springdemo.exception.handler;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class CustomerRestExceptionHandler {
+
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(CustomerNotFoundException ex){
+		
+		CustomerErrorResponse customerErrorResponse= new CustomerErrorResponse(HttpStatus.NOT_FOUND.value(),
+				ex.getMessage(),System.currentTimeMillis());
+		
+		return new ResponseEntity<CustomerErrorResponse>(customerErrorResponse, HttpStatus.NOT_FOUND);
+		
+	} 
+	
+	@ExceptionHandler
+	public ResponseEntity<CustomerErrorResponse> handleException(Exception ex){
+		
+		CustomerErrorResponse customerErrorResponse= new CustomerErrorResponse(HttpStatus.BAD_REQUEST.value(),
+				ex.getMessage(),System.currentTimeMillis());
+		
+		return new ResponseEntity<CustomerErrorResponse>(customerErrorResponse, HttpStatus.BAD_REQUEST);
+		
+	}
+	
+}
